6f8097a9642ca53091c80db141641cf4a0457fe3
alsino <iamahorse@gmail.com>
2020-03-30 22:47:30 +0200

    controls added

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
 
-// API doc here: https://f2kayjqpni.execute-api.eu-central-1.amazonaws.com/dev/
 // Inspiration: http://gabgoh.github.io/COVID/
 // Current: https://covid19-simulation.herokuapp.com/
+// Svelte tabs: https://svelte.dev/repl/8e68120858e5322272dc9136c4bb79cc?version=3.5.1
 
 // Issues
 // Data loading per promise not per timeout
@@ -13,6 +13,7 @@
 	import Intro from './intro.svelte';
 	import ChartSteps from './charts/chartSteps.svelte';
 	import ChartLine from './charts/chartLine.svelte';
+	import Control from './Control.svelte';
 	import { requestAPI } from './store.js';
 	import { params } from './store.js';
 	import { dataLoaded } from './store.js';
@@ -68,80 +69,50 @@
 	});
 
 
-	function handleClick() {
-		console.log(API)
-	}
-
-	function update(){
-
-		API= `https://f2kayjqpni.execute-api.eu-central-1.amazonaws.com/dev/simulate?
-		policy_period0=${settings.policy_period0}&
-		policy_period1=${settings.policy_period1}&
-		policy_period2=${settings.policy_period2}&
-		policy_period3=${settings.policy_period3}&
-		policy_period4=${settings.policy_period4}&
-		policy_strength1=${settings.policy_strength1}&
-		policy_strength2=${settings.policy_strength2}&
-		policy_strength3=${settings.policy_strength3}&
-		r0=${settings.r0}&
-		t_incubation=${settings.t_incubation}&
-		t_presymptomatic=${settings.t_presymptomatic}&
-		t_recovery_asymptomatic=${settings.t_recovery_asymptomatic}&
-		t_recovery_mild=${settings.t_recovery_mild}&
-		t_home_severe=${settings.t_home_severe}&
-		t_hospital_severe_recovered=${settings.t_hospital_severe_recovered}&
-		t_hospital_severe_deceased=${settings.t_hospital_severe_deceased}&
-		p_asymptomatic=${settings.p_asymptomatic}&
-		p_severe=${settings.p_severe}&
-		p_fatal=${settings.p_fatal}&
-		p_self_quarantine=${settings.p_self_quarantine}&
-		p_icu_given_hospital=${settings.p_icu_given_hospital}&
-		population_size=${settings.population_size}&
-		hospital_capacity=${settings.hospital_capacity}&
-		icu_capacity=${settings.icu_capacity}&
-		T=${settings["T"]}&
-		S=${settings["S"]}&
-		E=${settings["E"]}&
-		I=${settings["I"]}&
-		I_asymptomatic=${settings.I_asymptomatic}&
-		I_mild=${settings.I_mild}&
-		I_severe_home=${settings.I_severe_home}&
-		I_severe_hospital=${settings.I_severe_hospital}&
-		I_fatal_home=${settings.I_fatal_home}&
-		I_fatal_hospital=${settings.I_fatal_hospital}&
-		R_from_asymptomatic=${settings.R_from_asymptomatic}&
-		R_from_mild=${settings.R_from_mild}&
-		R_from_severe=${settings.R_from_severe}&
-		Dead=${settings.Dead}&
-		Hypothetical%20R0=${settings["Hypothetical%20R0"]}`
-
-		requestAPI(API);
-	}
-
 </script>
 
 <div id="app">
 	<Intro/>
-	<ChartSteps id={"chart-steps"}/>
-	<ChartLine id={"chart-mask"}/>
-	<!-- <button on:click={handleClick}>Get data from API</button> -->
 
+	<div id="charts">
+		<ChartSteps id={"chart-steps"}/>
+		<ChartLine id={"chart-mask"}/>
+	</div>
+	
 
-	<label>
+	<div id="controls">
+		<Control label={"Beginning"} small={false} end={false}/>
+		<Control label={"Policy Intervention 1"} small={false} end={false}/>
+		<Control label={"Policy Intervention 2"} small={false} end={false}/>
+		<Control label={"Policy Intervention 3"} small={false} end={false}/>
+		<Control label={"P_self_quarantine"} small={true} end={false}/>
+		<Control label={"End of simulation"} small={false} end={true}/>
+	</div>
+	
+
+	<!-- <div>
 		<span>Beginning</span>
 		<input type=text bind:value={settings.policy_period1} on:change={update}>
 		<span>Policy Strength</span>
 		<input type=range bind:value={settings.policy_strength1} min=0 max=1 step=0.1 on:change={update}>
 		<span>{settings.policy_strength1}</span>
-	</label>
+	</div> -->
 
-	<!-- <label>
+	<!-- <div>
 		<span>Policy Intervention 1</span>
-		<input type=text bind:value={settings.policy_period2} on:change={update}>
+		<input type=text bind:value={settings.policy_period1} on:change={update}>
 		<span>Policy Strength</span>
-		<input type=range bind:value={settings.policy_strength2} min=0 max=1 step=0.1 on:change={update}>
-		<span>{settings.policy_strength2}</span>
-	</label> -->
+		<input type=range bind:value={settings.policy_strength1} min=0 max=1 step=0.1 on:change={update}>
+		<span>{settings.policy_strength1}</span>
+	</div>
+
+	<div>
+		<span>Policy Intervention 2</span>
+		<input type=text bind:value={settings.policy_period1} on:change={update}>
+		<span>Policy Strength</span>
+		<input type=range bind:value={settings.policy_strength1} min=0 max=1 step=0.1 on:change={update}>
+		<span>{settings.policy_strength1}</span>
+	</div> -->
 
 
 	<!-- <div>{JSON.stringify(data)}</div> -->
@@ -159,9 +130,16 @@
 
 
 <style type="text/scss">
+
 	#app {
 		margin: 2em 6em 0 6em;
+
+			#controls {
+				margin-top: 2em;
+			}
 	}
+
+	
 
 	@media (max-width: 640px) {
 		#app {
