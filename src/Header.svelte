3298eab4ad54cbacc44ef6e6fa84314ade4523eb
Maximilian Eber <maximilianeber@gmail.com>
2020-04-04 15:34:16 +0200

    Add policy implications

--- src/Header.svelte
+++ src/Header.svelte
@@ -10,7 +10,12 @@
         </p>
         
         <p>
-        Don't take the model too seriously - it is not a forecasting tool. We built it to illustrate why keeping the number of infections low is crucial even in the medium-term. 
+        Don't take the model too seriously - it is not a forecasting tool. 
+        We built it to illustrate why keeping the number of new infections low throughout the epidemic is crucial. 
+        For example, you can turn down policy strength in the post-lockdown period to 0.5 using the third slider below. 
+        This is weak enough to trigger exponential growth, eventually overwhelming the capacity of the healthcare system. 
+        Therefore, we should not ask "How long should we be in lockdown?". Instead, the key question is "How can we implement strong policy at minimal cost?".
         </p>
+
       </div>
 </section>
