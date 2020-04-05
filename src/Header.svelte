f369e1cb9ba5f8e0e3e06b160ab379e1dd1b53f9
Maximilian Eber <maximilianeber@gmail.com>
2020-04-05 15:16:13 +0200

    Incorporate (more) feedback and add warning (revision)
    * Adjust intro
    
    * Add warning for current maintenance period

--- src/Header.svelte
+++ src/Header.svelte
@@ -2,8 +2,12 @@
       <h2 id="intro-heading">Covid-19 Policy Simulations</h2>
       <div id="intro-text">
         <p>
+        Warning: The tool is currently under revision.
+        </p>
+
+        <p>
         This tool visualizes the impact of varying policy paths on the dynamics of an epidemic. 
-        You can adjust the duration and strength of policy interventions (social distancing, test-trace-quarantine) using the date fields and sliders below the graphs. 
+        You can adjust the duration and strength of policy interventions (social distancing, test-trace-quarantine, etc.) using the date fields and sliders below the graphs. 
         The underlying simulation is based on a modified <a href="https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SEIR_model">SEIR model</a>. 
         It is roughly calibrated to match Germany's experience with Covid-19. 
         For details, please refer to the <a href="https://github.com/maximilianeber/covid">code</a>. 
@@ -11,10 +15,15 @@
         
         <p>
         Don't take the model too seriously - it is not a forecasting tool. 
-        We built it to illustrate why keeping the number of new infections low throughout the epidemic is crucial. 
-        For example, you can turn down policy strength in the post-lockdown period to 0.5 using the third slider below. 
-        This is weak enough to exhaust the capacity of the healthcare system. 
-        Therefore, we should not ask "How long will we be in lockdown?". Instead, the key question is "How can we implement strong policy at minimal cost?".
+        We built it to illustrate that the epidemic will resume when the lockdown is lifted unless strong policy measures are in place to keep the number of new infections low. 
+        Most epidemiological models reach this conclusion. 
+        Therefore, the key question is "How can we implement strong policy at minimal cost?".
+        </p>
+
+        <p>
+        Usage: 
+        You can start your exploration by turning down policy strength in the post-lockdown period to 0.5 using the third slider below. 
+        This is weak enough to exhaust the capacity of the healthcare system.
         </p>
 
       </div>
